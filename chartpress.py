fd0329a90050d3094cf9837d0a8e3c0faade78f8
Erik Sundell <erik.i.sundell@gmail.com>
2019-10-19 13:04:56 +0200

    back to dev

--- chartpress.py
+++ chartpress.py
@@ -18,7 +18,7 @@ import docker
 from ruamel.yaml import YAML
 from ruamel.yaml.scalarstring import SingleQuotedScalarString
 
-__version__ = '0.4.2'
+__version__ = '0.4.3.dev'
 
 # name of the environment variable with GitHub token
 GITHUB_TOKEN_KEY = 'GITHUB_TOKEN'
