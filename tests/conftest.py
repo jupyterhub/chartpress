712c5da03bd841d02ce151fac44f82ae33a21ae8
Erik Sundell <erik.i.sundell@gmail.com>
2019-10-23 00:56:50 +0200

    Initial chartpress run test

--- tests/conftest.py
+++ tests/conftest.py
@@ -10,8 +10,10 @@ def git_repo():
     """A temporary git repo with """
     with tempfile.TemporaryDirectory() as temp_dir:
         copy_tree("tests/data", os.path.join(temp_dir))
-
         stashed_cwd = os.getcwd()
         os.chdir(temp_dir)
-        yield git.Repo.init(temp_dir)
+        repo = git.Repo.init(temp_dir)
+        repo.index.add(".")
+        repo.index.commit("initial commit")
+        yield repo
         os.chdir(stashed_cwd)
--- tests/test_repo_interactions.py
+++ tests/test_repo_interactions.py
@@ -1,7 +1,13 @@
 import os
 
+import chartpress
+
 def test_git_repo_fixture(git_repo):
     assert git_repo.working_dir == os.getcwd()
     assert os.path.isfile("chartpress.yaml")
     assert os.path.isfile("testchart/Chart.yaml")
     assert os.path.isfile("testchart/values.yaml")
+
+
+def test_chartpress_run(git_repo):
+    chartpress.main()
