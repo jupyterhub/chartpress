7883d7097a327dd39ba3c49c1fbfadee5ec5b447
Erik Sundell <erik.i.sundell@gmail.com>
2020-11-02 16:50:32 +0100

    Bump version: 0.7.0 → 0.7.1.dev

--- chartpress.py
+++ chartpress.py
@@ -20,7 +20,7 @@ import docker
 from ruamel.yaml import YAML
 from ruamel.yaml.scalarstring import SingleQuotedScalarString
 
-__version__ = '0.7.0'
+__version__ = '0.7.1.dev'
 
 # name of the environment variable with GitHub token
 GITHUB_TOKEN_KEY = 'GITHUB_TOKEN'
