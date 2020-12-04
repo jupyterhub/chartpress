c704de22dff059d2bef02d0d56cd65589b5af352
Erik Sundell <erik.i.sundell@gmail.com>
2020-12-04 12:47:03 +0100

    Bump version: 1.0.2.dev → 1.0.2

--- setup.py
+++ setup.py
@@ -26,7 +26,7 @@ with open("README.md") as f:
 
 setup(
     name='chartpress',
-    version='1.0.2.dev',
+    version='1.0.2',
     py_modules=['chartpress'],
     cmdclass=cmdclass,
     entry_points={
