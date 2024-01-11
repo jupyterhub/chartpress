0abc52626b7ebcfc39f3f4d58c35f373ce05f0d1
Simon Li <orpheus+devel@gmail.com>
2024-01-11 18:32:39 +0000

    Bump version: 2.1.1.dev → 2.2.0

--- chartpress.py
+++ chartpress.py
@@ -20,7 +20,7 @@ from tempfile import TemporaryDirectory
 import docker
 from ruamel.yaml import YAML
 
-__version__ = "2.1.1.dev"
+__version__ = "2.2.0"
 
 # name of the environment variable with GitHub token
 GITHUB_TOKEN_KEY = "GITHUB_TOKEN"
